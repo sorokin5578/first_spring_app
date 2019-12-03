2e182eeaa5f2811ae18ec9b39e30aaa14b2ef064
sorokin5578 <45458807+sorokin5578@users.noreply.github.com>
2019-12-03 17:27:05 +0200

    Structure with CRUD and correct input

--- src/test/java/com/example/lab_4sprg/controller/MainController.java
+++ src/test/java/com/example/lab_4sprg/controller/MainController.java
@@ -2,8 +2,11 @@ package com.example.lab_4sprg.controller;
 
 import com.example.lab_4sprg.domain.group443;
 import com.example.lab_4sprg.repos.group443Repo;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -50,6 +54,7 @@ public class MainController {
 
     @PostMapping("filter")
     public String filter(@RequestParam String filter, Map<String,Object> model){
+        try{
         Iterable<group443> names;
         if(filter!=null && !filter.isEmpty()){
             names =group443Repo.findByAge2(Integer.parseInt(filter));
@@ -58,6 +63,10 @@ public class MainController {
             names = group443Repo.findAllHql();
         }
         model.put("names", names);
+        }catch (Exception e){
+            model.put("message", "Введите целое число!");
+            return "main";
+        }
         return "main";
     }
 
@@ -68,12 +77,17 @@ public class MainController {
 
     @PostMapping("/Add")
     public String Add(@RequestParam String text, @RequestParam String text2, @RequestParam String text3, Map<String,Object> model){
-        group443 test1 = new group443(text, text2, Integer.parseInt(text3));
-        group443Repo.save(test1);
-        Iterable<group443> names = group443Repo.findAllHql();
-        model.put("message", "Cтудент успешно добавлен!");
-        model.put("names", names);
-
+        try {
+            group443 test1 = new group443(text, text2, Integer.parseInt(text3));
+            group443Repo.save(test1);
+            Iterable<group443> names = group443Repo.findAllHql();
+            model.put("message", "Cтудент успешно добавлен!");
+            model.put("names", names);
+        }
+        catch (Exception e){
+            model.put("messageAdd", "Не правильный формат ввода!");
+            return "Add";
+        }
         return "main";
     }
 
@@ -86,19 +100,37 @@ public class MainController {
     @Transactional
     @PostMapping("/DeleteById")
     public String DeleteById(@RequestParam String text, Map<String,Object> model){
-        //group443Repo.DeleteById(Integer.parseInt(text));
-        Session session = session();
-        Transaction txn= session.beginTransaction();
-        String hql="delete com.example.lab_4sprg.domain.group443 as gr where gr.id=:id";
-        Query query = session.createQuery(hql);
-        query.setParameter("id", Integer.parseInt(text));
-        query.executeUpdate();
-        txn.commit();
-        Iterable<group443> names = group443Repo.findAllHql();
-        model.put("message", "Студент с id="+text+" успешно удален!");
-        model.put("names", names);
+        try {
+            if (!text.isEmpty()) {
+                Session session = session();
+                group443 student = FindByIdCrit(session, Integer.parseInt(text));
+                if (student != null) {
+                    Transaction txn = session.beginTransaction();
+                    String hql = "delete com.example.lab_4sprg.domain.group443 as gr where gr.id=:id";
+                    Query query = session.createQuery(hql);
+                    query.setParameter("id", Integer.parseInt(text));
+                    query.executeUpdate();
+                    txn.commit();
+                    Iterable<group443> names = group443Repo.findAllHql();
+                    model.put("message", "Студент с id=" + text + " успешно удален!");
+                    model.put("names", names);
+                    return "main";
+                } else {
+                    model.put("messageDel", "Студента с id=" + text + " не существует!");
+                    return "DeleteById";
+                }
+            } else {
+                model.put("messageDel", "Не правильный id!");
+                return "DeleteById";
+            }
+        }catch (Exception e){
+            model.put("messageDel", "Не правильный формат ввода!");
+            return "DeleteById";
+        }
 
-        return "main";
+
+
+
     }
 
     @GetMapping("/UpdateById")
@@ -110,23 +142,50 @@ public class MainController {
     @Transactional
     @PostMapping("/UpdateById")
     public String UpdateById(@RequestParam String text0,@RequestParam String text1, @RequestParam String text2, @RequestParam String text3, Map<String,Object> model){
-        Session session =session();
-        Transaction txn= session.beginTransaction();
-        String hql="update com.example.lab_4sprg.domain.group443 as gr set gr.sname1=:sname, gr.name1=:name, gr.age=:age where gr.id=:id";
-        Query query = session.createQuery(hql);
-        query.setParameter("id", Integer.parseInt(text0));
-        query.setParameter("sname", text2);
-        query.setParameter("name", text1);
-        query.setParameter("age", Integer.parseInt(text3));
-        query.executeUpdate();
-        txn.commit();
-        Iterable<group443> names = group443Repo.findAllHql();
-        model.put("message", "Студент с id="+text0+" успешно обновлен!");
-        model.put("names", names);
+        try {
+            if (!text0.isEmpty()) {
+                Session session = session();
+                group443 student = FindByIdCrit(session, Integer.parseInt(text0));
+                if (student != null) {
+                    Transaction txn = session.beginTransaction();
+                    String hql = "update com.example.lab_4sprg.domain.group443 as gr set gr.sname1=:sname, gr.name1=:name, gr.age=:age where gr.id=:id";
+                    Query query = session.createQuery(hql);
+                    query.setParameter("id", Integer.parseInt(text0));
+                    text2 = text2.isEmpty() ? student.getSname1() : text2;
+                    query.setParameter("sname", text2);
+                    text1 = text1.isEmpty() ? student.getName1() : text1;
+                    query.setParameter("name", text1);
+                    text3 = text3.isEmpty() ? student.getAge().toString() : text3;
+                    query.setParameter("age", Integer.parseInt(text3));
+                    query.executeUpdate();
+                    txn.commit();
+                    Iterable<group443> names = group443Repo.findAllHql();
+                    model.put("message", "Студент с id=" + text0 + " успешно обновлен!");
+                    model.put("names", names);
+                    return "main";
+                } else {
+                    model.put("messageUp", "Студента с id=" + text0 + " не существует!");
+                    return "UpdateById";
+                }
+            } else {
+                model.put("messageUp", "Не правильный id!");
+                return "UpdateById";
+            }
+        }catch (Exception e){
+            model.put("messageUp", "Не правильный формат ввода!");
+            return "UpdateById";
+        }
 
-        return "main";
     }
 
+
+    public group443 FindByIdCrit(Session session, Integer id){
+        Transaction txn= session.beginTransaction();
+        Criteria criteria = session.createCriteria(group443.class).add(Restrictions.like("id",id));
+        group443 group = (group443)criteria.uniqueResult();
+        txn.commit();
+        return group;
+    }
 
     public Session session(){
         entityManager=entityManager.getEntityManagerFactory().createEntityManager();
--- src/test/java/com/example/lab_4sprg/repos/group443Repo.java
+++ src/test/java/com/example/lab_4sprg/repos/group443Repo.java
@@ -10,19 +10,19 @@ import java.util.List;
 
 public interface group443Repo extends CrudRepository<group443, Integer> {
 
-    @Query("from com.example.lab_4sprg.domain.group443 as gr where gr.age=:age")
+    @Query("from com.example.lab_4sprg.domain.group443 as gr where gr.age=:age ORDER BY gr.id ASC")
     List<group443> findByAge2(@Param("age")Integer age);
 
-    @Query("from com.example.lab_4sprg.domain.group443")
+    @Query("from com.example.lab_4sprg.domain.group443 as gr ORDER BY gr.id ASC")
     List<group443> findAllHql();
 
-    @Transactional
-    @Modifying
-    @Query("delete com.example.lab_4sprg.domain.group443 as gr where gr.id=:id")
-    List<group443> DeleteById(@Param("id")Integer id);
-
-    @Transactional
-    @Modifying
-    @Query("update com.example.lab_4sprg.domain.group443 as gr set gr.sname1=:sname, gr.name1=:name, gr.age=:age where gr.id=:id")
-    List<group443> UpdateById(@Param("id")Integer id,@Param("sname")String sname,@Param("name")String name,@Param("age")Integer age);
+//    @Transactional
+//    @Modifying
+//    @Query("delete com.example.lab_4sprg.domain.group443 as gr where gr.id=:id")
+//    List<group443> DeleteById(@Param("id")Integer id);
+//
+//    @Transactional
+//    @Modifying
+//    @Query("update com.example.lab_4sprg.domain.group443 as gr set gr.sname1=:sname, gr.name1=:name, gr.age=:age where gr.id=:id")
+//    List<group443> UpdateById(@Param("id")Integer id,@Param("sname")String sname,@Param("name")String name,@Param("age")Integer age);
 }
